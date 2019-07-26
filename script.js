cc74f9c290214411aa2c0eb956a5de9a4d59d9a8
Stella Kim <testcodesk@gmail.com>
2019-07-26 17:09:12 -0400

    1st attemp

---
+++ script.js
@@ -0,0 +1,38 @@
+const API = 'https://acme-users-api-rev.herokuapp.com/api/users/search/';
+
+const input = document.querySelector('input[type="search"]');
+const tbody = document.querySelector('tbody');
+const clear = document.querySelector('#clear');
+const thead = document.querySelector('thead');
+
+input.addEventListener('search', () => {
+    let html = ''
+    window.location.hash = input.value;
+
+    // tbody.innerHTML = '<td><tr>test</tr></td>'
+    thead.style.visibility = "visible";
+    fetch(`${API}${input.value}`)
+        .then (response => response.json())
+            .then( data => data.users.forEach((user)=>{
+                // console.log(user.firstName)
+                html = `<tr><td><img src='${user.avatar}' alt='user image'</td>
+                            <td>${user.firstName}</td>
+                            <td>${user.lastName}</td>
+                            <td>${user.email}</td>
+                            <td>${user.title}</td></tr>`;
+                
+                tbody.innerHTML += html;
+            }));
+});
+
+
+
+clear.addEventListener('click', () => {
+    window.location.hash = ''
+    const cells = document.getElementById('user-table').getElementsByTagName(tr);
+    //clear all the cells
+    for( let cell of cells){
+        tbody.removeChild(tbody.cell);
+    }
+})
+
